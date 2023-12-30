195a60da739e2fc587c46dab01532db112ec4ae9
SOUL X SYSTEM <154720925+mahtoanjali@users.noreply.github.com>
2023-12-30 22:33:39 +0530

    Update genStr.py

--- genStr.py
+++ genStr.py
@@ -37,7 +37,7 @@ async def genStr(_, msg: Message):
     if msg.chat.id in Credentials.BANNED_USERS:
         await bot.send_message(
             chat_id=msg.chat.id,
-            text="You are Banned. Contact My [Support Group](https://t.me/safothebot)",
+            text="You are Banned. Contact My [Support Group](https://t.me/AnjalixSupportxGroup)",
             reply_to_message_id=msg.message_id
         )
         return
@@ -49,7 +49,7 @@ async def genStr(_, msg: Message):
             if user.status == "kicked":
                await bot.send_message(
                    chat_id=msg.chat.id,
-                   text="Sorry Sir, You are Banned. Contact My [Support Group](https://t.me/safothebot).",
+                   text="Sorry Sir, You are Banned. Contact My [Support Group](https://t.me/AnjalixSupportxGroup).",
                    parse_mode="markdown",
                    disable_web_page_preview=True
                )
@@ -71,7 +71,7 @@ async def genStr(_, msg: Message):
         except Exception:
             await bot.send_message(
                 chat_id=msg.chat.id,
-                text="**Something Went Wrong. Contact My [Support Group](https://t.me/safothebot).**",
+                text="**Something Went Wrong. Contact My [Support Group](https://t.me/AnjalixSupportxGroup).**",
                 parse_mode="markdown",
                 disable_web_page_preview=True
             )
